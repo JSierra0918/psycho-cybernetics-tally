5cb12c9cb13437aabd5022d04874c5e3db1f010f
Jorge Sierra <3d.jorgesierra@gmail.com>
2020-02-22 17:05:41 -0500

    need to create a button for adding the user

--- pct/src/app/tally/tally.component.ts
+++ pct/src/app/tally/tally.component.ts
@@ -1,36 +1,77 @@
 import { Component, OnInit } from "@angular/core";
 import { getLocaleDateTimeFormat } from '@angular/common';
 
+// import userDataJ from '../localStorage/userData.json';
+
 //decorator
 @Component({
     selector: 'app-tally',
     templateUrl: 'tally.html',
     styleUrls: ['tally.scss'],
-  })
+})
 
-  export class Tally implements OnInit {
-    title: string;
-    count: number;
+export class Tally implements OnInit {
     userData: any[];
-    id: number;
-    //date created
-    dateTime: Date = new Date();
-    dateCreated: string
 
-    constructor() {}
+    // template Tutorial
+    loginText = 'Login';
+    signUpText = 'Sign Up';
+    lessons:Array<String> = ['Lesson 1', 'Lessons 2'];
+
+    login() {
+        console.log('Login');
+    }
+
+    signUp() {
+        console.log('Sign Up');
+        console.log(`Today is ${new Intl.DateTimeFormat('en-US').format(new Date())}`);
+    }
+    //====================================================================== TEMPLATED END
+
+    constructor() { }
 
     ngOnInit() {
-        this.title = "This is the Tally title";
-        this.count = 3;
-        this.id = this.id + 1;
-        this.userData = [
-            {
-                id: this.id,
-                title: this.title,
-                count: this.count,
-                created: `${this.dateTime.getUTCMonth() +1}/${this.dateTime.getDay}/${this.dateTime.getUTCFullYear()}`
-            }
+        const dateTime: Date = new Date();
+        const created: String = `${dateTime.getUTCMonth() + 1} - ${dateTime.getDay} - ${dateTime.getUTCFullYear()}`;
+        console.log(`${dateTime.getUTCMonth() + 1} - ${dateTime.getDay} - ${dateTime.getUTCFullYear()}`);
+        console.log("hello")
 
+        this.userData = 
+        [
+            {
+                id: 0,
+                title: "Title1",
+                count: 0,
+                created: created
+            },
+            {
+                id: 1,
+                title: "Title2",
+                count: 0,
+                created: created
+            }
         ]
     }
-  }
+
+    increaseCount(e,index: number) {
+        const specificUserData = this.userData[index];
+        console.log('specificUserData:', specificUserData);
+        console.log(e.target, e.target.count)
+        specificUserData.count++;
+    }
+
+    tapEvent(e) {
+        console.log(e);
+        console.log("TAP!");
+    }
+
+    goToStats(e:Event, index: number) {
+        const specificUserData = this.userData[index];
+        console.log(e);
+        //workaround for stopPropagation not working.
+        specificUserData.count--;
+        // e.stopPropagation();
+        //send user to statistics page
+
+    }
+}
